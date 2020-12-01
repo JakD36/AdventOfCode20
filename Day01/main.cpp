a1e892448db6a9840f653159f3221a992fb2c8bf
Jack Davidson <jakd36@gmail.com>
2020-12-01 20:32:52 +0000

    Completed Day 01 part 1

---
+++ Day01/main.cpp
@@ -0,0 +1,84 @@
+#include <vector>
+#include <algorithm>
+#include <chrono>
+
+inline int FindSolutionFromBruteForce(std::vector<int> &inputs);
+inline int FindSolutionFromSortedInputs(std::vector<int> &inputs);
+
+int main() {
+    auto start = std::chrono::steady_clock::now();
+
+    char filepath[] = "input.txt";
+    FILE* file;
+    file = fopen(filepath,"r");
+
+    std::vector<int> inputs;
+    inputs.reserve(300);
+
+    if(file)
+    {
+        int val;
+        while(fscanf(file,"%d\n",&val) != EOF)
+        {
+            inputs.push_back(val);
+        }
+
+        std::sort(begin(inputs),end(inputs));
+        int solution = FindSolutionFromSortedInputs(inputs);
+//        int solution = FindSolutionFromBruteForce(inputs);
+        printf("%d\n",solution);
+
+        fclose(file);
+    }
+    else
+    {
+        printf("Failed to open %s",filepath);
+    }
+
+    auto end = std::chrono::steady_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    printf("%d microseconds\n",duration);
+    return 0;
+}
+
+inline int FindSolutionFromBruteForce(std::vector<int> &inputs)
+{
+    for(int a : inputs)
+    {
+        for(int b : inputs)
+        {
+            int sum = a + b;
+            if(sum == 2020)
+            {
+                return a * b;
+            }
+        }
+    }
+    return -1;
+}
+
+inline int FindSolutionFromSortedInputs(std::vector<int> &inputs)
+{
+    for(int a : inputs)
+    {
+        for(int b : inputs)
+        {
+            int sum = a + b;
+            int diff = sum - 2020;
+            if(diff < 0)
+            {
+                continue;
+            }
+            else if(diff == 0)
+            {
+                // found the solution
+                return a * b;
+            }
+            else
+            {
+                break; // Our sum is greater than 2020 no solution after will work
+            }
+        }
+    }
+    return -1;
+}
