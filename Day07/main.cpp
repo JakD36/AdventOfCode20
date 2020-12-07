c94f3abc59f63fce03ae775bb0b57584a65027f7
Jack Davidson <jakd36@gmail.com>
2020-12-07 22:53:33 +0000

    Completed Day07 Part 1, threw it together its nothing great

---
+++ Day07/main.cpp
@@ -0,0 +1,105 @@
+#include <vector>
+#include <algorithm>
+#include <chrono>
+#include <string>
+#include <unordered_map>
+#include <set>
+#include <queue>
+
+void Part1(FILE* file);
+void Part2(FILE* file);
+
+class Bag{
+public:
+    std::string Name;
+    std::vector<Bag*> Parents;
+    std::vector<Bag*> Children;
+
+    Bag(std::string name)
+    {
+        Name = name;
+    }
+};
+
+int main() {
+    auto start = std::chrono::steady_clock::now();
+
+    char filepath[] = "input.txt";
+    FILE* file;
+    file = fopen(filepath,"r");
+
+    if(file)
+    {
+        Part1(file);
+    }
+    else
+    {
+        printf("Failed to open %s\n",filepath);
+    }
+
+    auto end = std::chrono::steady_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    printf("%d microseconds\n",duration);
+    return 0;
+}
+
+Bag* FindOrCreate(std::unordered_map<std::string,Bag*> &map, std::string name)
+{
+    auto current = map.find(name);
+    Bag* bag = nullptr;
+    if(current == end(map))
+    {
+        bag = new Bag(name);
+        map.emplace(name,bag);
+    }
+    else
+    {
+        bag = current->second;
+    }
+    return bag;
+}
+
+void Part1(FILE* file)
+{
+
+    // Build our tree
+    char buffer[30];
+    char buffer2[30];
+    char buffer3[128];
+    buffer[0] = '\0';
+    buffer2[0] = '\0';
+    buffer3[0] = '\0';
+    std::unordered_map<std::string,Bag*> map;
+    while(fscanf(file,"%s %s %*s %*s %[^\n]",buffer,buffer2,buffer3) != EOF)
+    {
+        std::string name(buffer); name.append(" ").append(buffer2);
+        Bag* bag = FindOrCreate(map,name);
+
+        int offset = 0;
+        int count, bytesRead;
+        char b1[30] = "\0";
+        char b2[30] = "\0";
+        while(sscanf(&buffer3[offset], "%d %s %s %*s %n",&count,b1,b2,&bytesRead) != EOF)
+        {
+            std::string subBag(b1); subBag.append(" ").append(b2);
+            Bag* sub = FindOrCreate(map,subBag);
+            sub->Parents.push_back(bag);
+            offset += bytesRead;
+        }
+    }
+
+    // count
+    std::set<Bag*> uniqueBagsContaining;
+    auto gold = map.find("shiny gold");
+    std::queue<Bag*> queue;
+    queue.push(gold->second);
+    while (queue.size() > 0)
+    {
+        auto next = queue.front();
+        queue.pop();
+        for(auto parent : next->Parents){queue.push(parent);}
+        if(next->Name != "shiny gold")
+            uniqueBagsContaining.emplace(next);
+    }
+    printf("%d\n",uniqueBagsContaining.size());
+}
