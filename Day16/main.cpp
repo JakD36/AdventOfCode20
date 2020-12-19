cba5cc9b8ba5166bb71a03b12b5f121f6f70aeee
Jack Davidson <jakd36@gmail.com>
2020-12-19 18:54:15 +0000

    Completed day 16 part 2

--- Day16/main.cpp
+++ Day16/main.cpp
@@ -5,28 +5,29 @@
 #include <numeric>
 
 void Part1(FILE* file);
-void Part2(FILE* file);
+void Part2(FILE* ticketRanges, FILE* validTickets);
+void Part2_RemoveDuds(FILE* file);
 
 int main() {
     auto start = std::chrono::steady_clock::now();
 
-    char filepath[] = "input.txt";
-    FILE* file;
-    file = fopen(filepath,"r");
+    FILE* f1 = fopen("ticketRanges.txt","r");
+    FILE* f2 = fopen("validTickets.txt","r");
 
-    if(file)
+    if(f1 && f2)
     {
-        Part1(file);
+        Part2(f1,f2);
     }
     else
     {
-        printf("Failed to open %s\n",filepath);
+        printf("Failed to open files\n");
     }
 
     auto end = std::chrono::steady_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
     printf("%d microseconds\n",duration);
-    fclose(file);
+    fclose(f1);
+    fclose(f2);
     return 0;
 }
 
@@ -35,6 +36,9 @@ struct Field
 public:
     char m_name[255];
     int x1,x2,x3,x4;
+
+    unsigned int matchCount;
+    unsigned int match;
 
     bool IsValid(int val)
     {
@@ -113,7 +117,6 @@ void Part1(FILE* file) // between 28998 - 29022
                     if(!valid)
                     {
                         sum += val;
-                        printf("%d\n",val);
                         invalidVals.push_back(val);
                     }
                     offset += bytesRead;
@@ -126,7 +129,7 @@ void Part1(FILE* file) // between 28998 - 29022
     printf("sum %d\n",sum);
 }
 
-void Part2(FILE* file) // between 28998 - 29022
+void Part2_RemoveDuds(FILE* file)
 {
     std::vector<int> invalidVals;
     std::vector<Field> fields;
@@ -135,7 +138,7 @@ void Part2(FILE* file) // between 28998 - 29022
 
     std::vector<int> ticketNumbers;
 
-    int sum = 0;
+    FILE* writeFile = fopen("validTickets.txt","w");
 
     while(fgets(buf,255,file) != nullptr)
     {
@@ -176,6 +179,7 @@ void Part2(FILE* file) // between 28998 - 29022
                 int offset = 0;
                 int val = 0, bytesRead = 0;
                 int end = std::find(buf,buf+255,'\0') - buf;
+                bool failed = false;
                 while(offset < end && sscanf(&buf[offset],"%d,%n",&val,&bytesRead) > 0)
                 {
                     bool valid = false;
@@ -189,16 +193,105 @@ void Part2(FILE* file) // between 28998 - 29022
                     }
                     if(!valid)
                     {
-                        sum += val;
-                        printf("%d\n",val);
-                        invalidVals.push_back(val);
+                        failed = true;
+                        break;
                     }
                     offset += bytesRead;
                 }
+                if(!failed)
+                    fprintf(writeFile,"%s", buf);
             }
         }
     }
+    fclose(writeFile);
+}
 
-    printf("acc %d\n",std::accumulate(begin(invalidVals),end(invalidVals),0));
-    printf("sum %d\n",sum);
+void Part2(FILE* ticketRanges, FILE* validTickets)
+{
+    unsigned int all = 0;
+    for(unsigned int i = 0; i < 20; ++i)
+        all |= 1U << i;
+
+    std::vector<Field> fields;
+    char key[255];
+    int x1 = 0,x2 = 0,x3 = 0,x4 = 0;
+    while(fscanf(ticketRanges,"%[^:]: %d-%d or %d-%d\n",key,&x1,&x2,&x3,&x4) != EOF)
+    {
+        Field tmp;
+        strcpy(tmp.m_name,key);
+        tmp.x1 = x1; tmp.x2 = x2; tmp.x3 = x3; tmp.x4 = x4;
+        tmp.match = all;
+        tmp.matchCount = 0;
+        fields.push_back(tmp);
+    }
+
+    char buf[255];
+    while(fgets(buf,255,validTickets) != nullptr)
+    {
+        int offset = 0;
+        int val = 0, bytesRead = 0;
+        int end = std::find(buf,buf+255,'\0') - buf;
+        unsigned int index = 0;
+        while(offset < end && sscanf(&buf[offset],"%d,%n",&val,&bytesRead) > 0)
+        {
+            for(int i = 0, count = fields.size(); i < count; ++i)
+            {
+                fields[i].match &= ~(!fields[i].IsValid(val) << index);
+            }
+            ++index;
+            offset += bytesRead;
+        }
+    }
+
+    // Count the number of valid values in each, so we can sort by what has the least
+    for(unsigned int i = 0; i < 20; ++i)
+    {
+        for(unsigned int j = 0; j < 20; ++j)
+        {
+            fields[i].matchCount += (fields[i].match & (1U << j)) > 0;
+        }
+    }
+
+    std::sort(begin(fields),end(fields),[&](auto& a, auto& b){return b.matchCount>a.matchCount;});
+
+    int yourTicket[] = {67,107,59,79,53,131,61,101,71,73,137,109,157,113,173,103,83,167,149,163};
+    unsigned long total = 1;
+    // Now our list is sorted we can start eliminating possibilities
+    unsigned int found = 0;
+    for(int i = 0; i < 20; ++i)
+    {
+//        for(int j = 0; j < 20; ++j)
+//        {
+//            if ((fields[i].match & (1 << j)) > 0)
+//            {
+//                printf("1 ");
+//            } else
+//                printf("  ");
+//        }
+//        printf("\n");
+        fields[i].match &= ~found;
+//        for(int j = 0; j < 20; ++j)
+//        {
+//            if ((fields[i].match & (1 << j)) > 0)
+//            {
+//                printf("1 ");
+//            } else
+//                printf("  ");
+//        }
+//        printf("\n");
+        found |= fields[i].match;
+        for(int j = 0; j < 20; ++j)
+        {
+            if ((fields[i].match & (1 << j)) > 0)
+            {
+                if(strncmp(fields[i].m_name,"departure",9) == 0)
+                {
+                    total *= yourTicket[j];
+                }
+                printf("%s %d\n",fields[i].m_name,j);
+            }
+        }
+//        printf("\n\n");
+    }
+    printf("Total %ld\n",total);
 }
