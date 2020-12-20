653aee99723e189ffa478c3a9766e178acbc319e
Jack Davidson <jakd36@gmail.com>
2020-12-20 14:13:09 +0000

    Completed Day 17 Part 1 and 2, using a more simplified bool[][][][]!

--- Day17/main.cpp
+++ Day17/main.cpp
@@ -5,15 +5,16 @@
 #include <numeric>
 
 void Part1(FILE* file);
+void Part2(FILE* file);
 
 int main() {
     auto start = std::chrono::steady_clock::now();
 
-    FILE* file = fopen("testInput.txt","r");
+    FILE* file = fopen("input.txt","r");
 
     if(file)
     {
-        Part1(file);
+        Part2(file);
     }
     else
     {
@@ -29,20 +30,16 @@ int main() {
 
 void Part1(FILE* file)
 {
-    unsigned long read[64][64], write[64][64];
-    std::fill((unsigned long*)read, (unsigned long*)read + 64 * 64, 0lu);
-    std::fill((unsigned long*)write, (unsigned long*)write + 64 * 64, 0lu);
-    for(int i = 0; i < 64; ++i)
-    {
-        for(int j = 0; j < 64; ++j)
-        {
-            read[i][j] = 0;
-            write[i][j] = 0;
-        }
-    }
+    const int width = 32;
+    const int halfWidth = width / 2;
+
+    bool read[width][width][width], write[width][width][width];
+
+    std::fill((bool*)read, (bool*)read + width * width * width, false);
+    std::fill((bool*)write, (bool*)write + width * width * width, false);
 
     char buf[255];
-    int z = 0;
+    int j = 0;
     while(fgets(buf,255,file) != nullptr)
     {
         int i = 0;
@@ -50,22 +47,22 @@ void Part1(FILE* file)
         {
             if(buf[i] == '#')
             {
-                read[32][28 + i] |= (1lu << (28lu + z));
-                write[32][28 + i] |= (1lu << (28lu + z));
+                read[halfWidth][halfWidth + i][halfWidth+j] = true;
+                write[halfWidth][halfWidth + i][halfWidth+j] = true;
             }
             ++i;
         }
-        ++z;
+        ++j;
     }
 
     int offset = 1;
     for(int rounds = 0; rounds < 6; ++rounds)
     {
-        for(int y = offset; y < 64-offset; ++y)
+        for(int y = offset; y < width-offset; ++y)
         {
-            for(int x = offset; x < 64-offset; ++x)
+            for(int x = offset; x < width-offset; ++x)
             {
-                for(unsigned long z2 = offset; z2 < 64 - offset; ++z2)
+                for(int z = offset; z < width - offset; ++z)
                 {
 
                     int sum = 0;
@@ -73,40 +70,131 @@ void Part1(FILE* file)
                     {
                         for(int j = x-1; j <= x+1; ++j)
                         {
-                            for(unsigned int k = z2 - 1; k <= z2 + 1; ++k)
+                            for(unsigned int k = z - 1; k <= z + 1; ++k)
                             {
-                                sum += (read[i][j] & (1lu << k)) > 0lu;
+                                sum += read[i][j][k];
                             }
                         }
                     }
 
-                    bool active = (read[y][x] & (1lu << z2)) > 0lu;
+                    bool active = read[y][x][z];
                     sum -= active;
 
                     if(active && !(sum == 2 || sum == 3))
                     {
-                        write[y][x] &= ~(1lu << z2);
+                        write[y][x][z] = false;
                     }
                     else if(!active && sum == 3)
                     {
-                        write[y][x] |= (1u << z2);
+                        write[y][x][z] = true;
                     }
 
                 }
             }
         }
 
-        memcpy((unsigned long*)read,(unsigned long*)write,64*64 * sizeof(unsigned long));
+        memcpy((bool*)read,(bool*)write,width * width * width * sizeof(bool));
     }
 
     int total = 0;
-    for(int y = 0; y < 64; ++y)
+    for(int y = 0; y < width; ++y)
     {
-        for (int x = 0; x < 64; ++x)
+        for (int x = 0; x < width; ++x)
         {
-            for (unsigned long z = 0; z < 64; ++z)
+            for (unsigned long z = 0; z < width; ++z)
             {
-                total += (read[y][x] & (1lu << z)) > 0;
+                total += read[y][x][z];
+            }
+        }
+    }
+    printf("%d\n",total);
+}
+
+void Part2(FILE* file)
+{
+    const int width = 32;
+    const int halfWidth = width / 2;
+
+    bool read[width][width][width][width], write[width][width][width][width];
+
+    std::fill((bool*)read, (bool*)read + width * width * width * width, false);
+    std::fill((bool*)write, (bool*)write + width * width * width * width, false);
+
+    char buf[255];
+    int j = 0;
+    while(fgets(buf,255,file) != nullptr)
+    {
+        int i = 0;
+        while(buf[i] != '\0' && buf[i] != '\n')
+        {
+            if(buf[i] == '#')
+            {
+                read[halfWidth][halfWidth][halfWidth + i][halfWidth+j] = true;
+                write[halfWidth][halfWidth][halfWidth + i][halfWidth+j] = true;
+            }
+            ++i;
+        }
+        ++j;
+    }
+
+    int offset = 1;
+    for(int rounds = 0; rounds < 6; ++rounds)
+    {
+        for(int y = offset; y < width-offset; ++y)
+        {
+            for(int x = offset; x < width-offset; ++x)
+            {
+                for(int z = offset; z < width - offset; ++z)
+                {
+                    for(int w = offset; w < width - offset; ++w)
+                    {
+
+                        int sum = 0;
+                        for(int i = y-1; i <= y+1; ++i)
+                        {
+                            for(int j = x-1; j <= x+1; ++j)
+                            {
+                                for(int k = z - 1; k <= z + 1; ++k)
+                                {
+                                    for(int l = w - 1; l <= w + 1; ++l)
+                                    {
+                                        sum += read[i][j][k][l];
+                                    }
+                                }
+                            }
+                        }
+
+                        bool active = read[y][x][z][w];
+                        sum -= active;
+
+                        if(active && !(sum == 2 || sum == 3))
+                        {
+                            write[y][x][z][w] = false;
+                        }
+                        else if(!active && sum == 3)
+                        {
+                            write[y][x][z][w] = true;
+                        }
+
+                    }
+                }
+            }
+        }
+
+        memcpy((bool*)read,(bool*)write,width * width * width * width * sizeof(bool));
+    }
+
+    int total = 0;
+    for(int y = 0; y < width; ++y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            for (int z = 0; z < width; ++z)
+            {
+                for (int w = 0; w < width; ++w)
+                {
+                    total += read[y][x][z][w];
+                }
             }
         }
     }
